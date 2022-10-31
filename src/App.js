c86048c57425f6510d552e8217bd780df7e55175
Ainis Taranda <taranda.ainis@gmail.com>
2022-10-31 17:06:50 -0400

    using props, useeffect, and useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import SingleCoffee from "./SingleCoffee";
 
 function App() {
+  const [data, setData] = useState([{ name: "", price: "" }]);
+  
+
+  useEffect(() => {
+    getCoffee()
+  }, [])
+
+  const getCoffee = () => {
+    fetch("https://api.sampleapis.com/coffee/hot")
+      .then((response) => response.json()) //convert response to json
+      .then((data) => setData(data)) //get formatted data
+      .catch((err) => console.log(err));
+  };
+
+  console.log("my data", data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Coffee by Sample API</h1>
+        <button onClick={() => getCoffee()}>Get me Coffees</button>
+        <div className="menu-items">
+          {data.map(item => {
+            console.log("each item", item);
+            return <SingleCoffee entireObject={item}/>;
+          })}
+
+          {/* <SingleCoffee
+            name="Black American Coffee"
+            price="$$$$"
+            description="delish"
+          />
+          <SingleCoffee name="Black Cubano" price="$$$$" description="delish" />
+          <SingleCoffee />
+          <SingleCoffee />
+          <SingleCoffee />
+          <SingleCoffee />
+          <SingleCoffee /> */}
+
+          {/* <div className="menu-items__item">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/640px-A_small_cup_of_coffee.JPG"
+              alt=""
+            ></img>
+            <div>
+              <h3>1. Black American Coffee</h3>
+              <p>Black coffee is as simple as it get.</p>
+            </div>
+          </div> */}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/SingleCoffee.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function SingleCoffee({entireObject}) {
+    const [favorite,setFavorite] = useState(false)
+    
+    
+//   const exampleProps = {
+//     name: "Black American Coffee",
+//     price: "$$$$",
+//     description: "delish",
+//   };
+//   const description = exampleProps.description
+//   const name = exampleProps.name
+//   const price = exampleProps.price
+
+//   const {description, name, price} = exampleProps //destructuring
+
+  // console.log('props here -->', name) // the entire props object
+  return (
+    <div className="menu-items__item" style={{ backgroundColor: favorite ? 'red' : 'white'}}>
+    
+      <img
+        src={entireObject.image}
+        alt="pictures of each coffee type"
+      ></img>
+      <div>
+        <h3>{entireObject.title}</h3>
+        <span>Price: {entireObject.price}</span>
+        <p>{entireObject.description}</p>
+        <button onClick={() => setFavorite(true)}>Favorite</button>
+       
+      </div>
+    </div>
+  );
+}
